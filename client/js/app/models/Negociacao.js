aa5fac3605095860f1d79b943d2649676e123abf
olvfg <GBFOLV>
2018-09-23 22:25:16 -0300

    Regras de Negociação

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,26 @@
+//nome do arquivo com letra maiuscula porque é uma classe
+
+class Negociacao {
+
+    constructor(data, quantidade, valor) {
+        this._data = new Date(data.getTime()); //this = variável implicita | _ atributos nao podem ser alterados
+        this._quantidade = quantidade;
+        this._valor = valor;
+        Object.freeze(this);
+    }
+
+    get volume() { //método (função dentro de uma classe)
+        return this._quantidade * this._valor;
+    }
+
+    //encapsulamento
+    get data() {
+        return new Date(this._data.getTime());
+    }
+    get quantidade () {
+        return this._quantidade;
+    }
+    get valor () {
+        return this._valor;
+    }
+}
